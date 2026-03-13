e7007ebca89d9329ce3aca30ef489a682e473f10
aniket866 <iamaniketkumarmaner@gmail.com>
2026-03-13 17:40:19 +0530

    code-rabbit-follow-up

--- src/interfaces/IIdentityToken.sol
+++ src/interfaces/IIdentityToken.sol
@@ -1,24 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-interface IIdentityToken {
-    function mint() external returns (uint256);
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { IERC721Metadata } from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
+interface IIdentityToken is IERC721, IERC721Metadata {
     function setAttribute(uint256 tokenId, string calldata key, bytes calldata value) external;
 
     function endorse(uint256 fromTokenId, uint256 toTokenId, bytes32 connectionType, uint256 validUntil) external;
-
-    function ownerOf(uint256 tokenId) external view returns (address);
-
-    function balanceOf(address owner) external view returns (uint256);
-
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
-
-    function tokenURI(uint256 tokenId) external view returns (string memory);
-
-    function supportsInterface(bytes4 interfaceId) external view returns (bool);
 
     function ownerToTokenId(address owner) external view returns (uint256);
 
